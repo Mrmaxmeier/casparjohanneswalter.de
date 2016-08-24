ec7c2cd66b83a73b82f56d4051d85b5378470d57
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-08-24 23:20:58 +0200

    yay for standards

--- research/kithara_math.js
+++ research/kithara_math.js
@@ -1,114 +1,113 @@
 
-export function gcd(a, b, depth=0) {
-	if (isNaN(a) || isNaN(b) || a == undefined || b == undefined) {
-		console.log(a, b, depth)
-		throw "a and/or b are undefined/NaN"
-	}
-	return (b == 0) ? a : gcd(b, a % b, depth+1)
+export function gcd (a, b, depth = 0) {
+  if (isNaN(a) || isNaN(b) || a === undefined || b === undefined) {
+    console.log(a, b, depth)
+    let err = 'a and/or b are undefined/NaN'
+    throw err
+  }
+  return (b === 0) ? a : gcd(b, a % b, depth + 1)
 }
 
-export function reduce(ratio) {
-	let gcd_ = gcd(ratio[0], ratio[1])
-	return [ratio[0] / gcd_, ratio[1] / gcd_]
+export function reduce (ratio) {
+  let gcd_ = gcd(ratio[0], ratio[1])
+  return [ratio[0] / gcd_, ratio[1] / gcd_]
 }
 
-export function mul(a, b) {
-	return reduce([a[0] * b[0], a[1] * b[1]])
+export function mul (a, b) {
+  return reduce([a[0] * b[0], a[1] * b[1]])
 }
 
-export function swp(f) {
-	return [f[1], f[0]]
+export function swp (f) {
+  return [f[1], f[0]]
 }
 
-export function div(a, b) {
-	return mul(a, swp(b))
+export function div (a, b) {
+  return mul(a, swp(b))
 }
 
-export function cpy(f) {
-	return [f[0], f[1]]
+export function cpy (f) {
+  return [f[0], f[1]]
 }
 
-export function repr(f) {
-	f = reduce(f)
-	return `${f[0]} / ${f[1]}`
+export function repr (f) {
+  f = reduce(f)
+  return `${f[0]} / ${f[1]}`
 }
 
-export function frac_to_cent(frac) {
-	return Math.log(frac[0] / frac[1]) / Math.log(Math.pow(2, 1 / 1200))
+export function fracToCent (frac) {
+  return Math.log(frac[0] / frac[1]) / Math.log(Math.pow(2, 1 / 1200))
 }
 
-
-function getMultiplier(obj) {
-	var x = obj.ratio[0]
-	if ((obj.ratio[0] / obj.ratio[1]) < 4 / 3) {
-		x *= 2
-	}
-	let octave = obj.octave ? obj.octave : 0
-	return reduce([ x * Math.pow(2, octave), obj.ratio[1] ])
+function getMultiplier (obj) {
+  let x = obj.ratio[0]
+  if ((obj.ratio[0] / obj.ratio[1]) < 4 / 3) {
+    x *= 2
+  }
+  let octave = obj.octave ? obj.octave : 0
+  return reduce([ x * Math.pow(2, octave), obj.ratio[1] ])
 }
 
-function calcOctave(frac) {
-	return Math.floor(Math.log(frac[0] / frac[1]) / Math.log(2))
+function calcOctave (frac) {
+  return Math.floor(Math.log(frac[0] / frac[1]) / Math.log(2))
 }
 
-export function calcState(state, obj) {
-	var multiplier
-	if (obj.index == 0) {
-		multiplier = div(obj.ratio, state[obj.index].ratio)
-		if (multiplier[0] / multiplier[1] < 1) {
-			multiplier[0] *= 2
-		}
-	} else {
-		multiplier = div(getMultiplier(obj), getMultiplier(state[obj.index]))
-	}
+export function calcState (state, obj) {
+  let multiplier
+  if (obj.index === 0) {
+    multiplier = div(obj.ratio, state[obj.index].ratio)
+    if (multiplier[0] / multiplier[1] < 1) {
+      multiplier[0] *= 2
+    }
+  } else {
+    multiplier = div(getMultiplier(obj), getMultiplier(state[obj.index]))
+  }
 
-	let passed_obj = obj
+  let passedObj = obj
 
-	console.log(`Multiplier: ${repr(multiplier)}`)
+  console.log(`Multiplier: ${repr(multiplier)}`)
 
-	return state.map((obj, index) => {
+  return state.map((obj, index) => {
+    let ratio = mul(getMultiplier(obj), multiplier)
+    let octave = calcOctave(ratio)
+    ratio = reduce(div(ratio, [Math.pow(2, octave), 1]))
+    if ((ratio[0] / ratio[1]) < 4 / 3) {
+      octave--
+      console.log(index, octave)
+    }
 
-		var ratio = mul(getMultiplier(obj), multiplier)
-		var octave = calcOctave(ratio)
-		ratio = reduce(div(ratio, [Math.pow(2, octave), 1]))
-		if ((ratio[0] / ratio[1]) < 4 / 3) {
-			octave--
-			console.log(index, octave)
-		}
+    if (index === 0) {
+      while ((ratio[0] / ratio[1]) >= 2) {
+        ratio[1] *= 2
+      }
+      ratio = reduce(ratio)
+    }
 
-		if (index == 0) {
-			while ((ratio[0] / ratio[1]) >= 2) {
-				ratio[1] *= 2
-			}
-			ratio = reduce(ratio)
-		}
+    if (passedObj.overtone) {
+      octave += Math.log2(passedObj.overtone / (multiplier[0] / multiplier[1]))
+    }
 
-		if (passed_obj.overtone) {
-			octave += Math.log2(passed_obj.overtone / (multiplier[0]/ multiplier[1]))
-		}
-
-		return {
-			ratio: ratio,
-			octave: octave,
-			overtone: passed_obj.overtone
-		}
-	})
+    return {
+      ratio: ratio,
+      octave: octave,
+      overtone: passedObj.overtone
+    }
+  })
 }
 
-export function calcOvertone(state, overtone) {
-	var ratio = mul(state.upperRow[0].ratio, [overtone, 1])
-	var octave = state.upperRow[0].octave
-	while ((ratio[0] / ratio[1]) >= 2) {
-		ratio[1] *= 2
-		octave++
-	}
-	ratio = reduce(ratio)
-	console.log(octave, ratio)
+export function calcOvertone (state, overtone) {
+  let ratio = mul(state.upperRow[0].ratio, [overtone, 1])
+  let octave = state.upperRow[0].octave
+  while ((ratio[0] / ratio[1]) >= 2) {
+    ratio[1] *= 2
+    octave++
+  }
+  ratio = reduce(ratio)
+  console.log(octave, ratio)
 
-	return calcState(state.upperRow, {
-		ratio: ratio,
-		octave: octave,
-		index: 0,
-		overtone: overtone
-	})
+  return calcState(state.upperRow, {
+    ratio: ratio,
+    octave: octave,
+    index: 0,
+    overtone: overtone
+  })
 }
--- research/partch_bruch_rechner.js
+++ research/partch_bruch_rechner.js
@@ -4,151 +4,155 @@ const ReactDOM = require('react-dom')
 
 import { RatioInput } from './kithara_components'
 import {
-	div,
-	mul,
-	reduce
+  div,
+  mul,
+  reduce
 } from './kithara_math'
 
-let range = num => Array.apply(null, Array(num)).map(function (_, i) {return i})
 let clone = obj => JSON.parse(JSON.stringify(obj))
-let highlighted = [[1,1], [81,80], [33,32], [21,20], [16,15], [12,11], [11,10], [10,9], [9,8], [8,7], [7,6], [32,27], [6,5], [11,9], [5,4], [14,11], [9,7], [21,16], [4,3], [27,20], [11,8], [7,5], [10,7], [16,11], [40,27], [3,2], [32,21], [14,9], [11,7], [8,5], [18,11], [5,3], [27,16], [12,7], [7,4], [16,9], [9,5], [20,11], [11,6], [15,8], [40,21], [64,33], [160,81], [2,1]]
+let highlighted = [[1, 1], [81, 80], [33, 32], [21, 20], [16, 15], [12, 11], [11, 10], [10, 9], [9, 8], [8, 7], [7, 6], [32, 27], [6, 5], [11, 9], [5, 4], [14, 11], [9, 7], [21, 16], [4, 3], [27, 20], [11, 8], [7, 5], [10, 7], [16, 11], [40, 27], [3, 2], [32, 21], [14, 9], [11, 7], [8, 5], [18, 11], [5, 3], [27, 16], [12, 7], [7, 4], [16, 9], [9, 5], [20, 11], [11, 6], [15, 8], [40, 21], [64, 33], [160, 81], [2, 1]]
 
-class Rechner extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			main: [1, 1],
-			scnd: [1, 1],
-			ops: [[12,11], [11,10], [10,9], [9,8], [8,7], [7,6], [6,5], [11,9], [5,4], [14,11], [9,7], [4,3], [11,8], [7,5], [10,7]]
-		}
-	}
-	outList(data) {
-		let isHighlighted = (e) => highlighted.reduce((a, d) => a || (d[0] == e[0] && d[1] == e[1]), false)
-		return (
-			<div>
-				<table>
-					<tbody>
-						<tr>
-							{data.map((d, i) => {
-								let data = !d ? null : d[0]
-								return <RatioInput key={i} data={data} isUpper={true} highlighted={isHighlighted(d)} disabled={true} />
-							})}
-						</tr>
-						<tr>
-							{data.map((d, i) => {
-								let data = !d ? null : d[1]
-								return <RatioInput key={i} data={data} isUpper={false} highlighted={isHighlighted(d)} disabled={true} />
-							})}
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		)
-	}
-	render() {
-		let clamp = (frac) => {
-			var steps = 0
-			while (frac[0] / frac[1] > 2.0) {
-				steps++
-				frac[1] *= 2
-				if (steps > 99)
-					return [null, null]
-			}
-			while (frac[0] / frac[1] < 1.0) {
-				steps++
-				frac[0] *= 2
-				if (steps > 99)
-					return [null, null]
-			}
-			return reduce(frac)
-		}
-		let sanetizeFrac = (frac) => {
-			let a = parseInt(frac[0])
-			let b = parseInt(frac[1])
-			if (isNaN(a) || isNaN(b))
-				return null
-			return [a, b]
-		}
-		return (
-			<div>
-				<table>
-					<tbody>
-						<tr><RatioInput data={this.state.main[0]} isUpper={true} changeCB={(v) => {
-							this.setState({main: [v, this.state.main[1]]})
-						}} /></tr>
-						<tr><RatioInput data={this.state.main[1]} changeCB={(v) => {
-							this.setState({main: [this.state.main[0], v]})
-						}} /></tr>
-					</tbody>
-				</table>
+export class Rechner extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      main: [1, 1],
+      scnd: [1, 1],
+      ops: [[12, 11], [11, 10], [10, 9], [9, 8], [8, 7], [7, 6], [6, 5], [11, 9], [5, 4], [14, 11], [9, 7], [4, 3], [11, 8], [7, 5], [10, 7]]
+    }
+  }
+  outList (data) {
+    let isHighlighted = (e) => highlighted.reduce((a, d) => a || (d[0] === e[0] && d[1] === e[1]), false)
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              {data.map((d, i) => {
+                let data = !d ? null : d[0]
+                return <RatioInput key={i} data={data} isUpper highlighted={isHighlighted(d)} disabled />
+              })}
+            </tr>
+            <tr>
+              {data.map((d, i) => {
+                let data = !d ? null : d[1]
+                return <RatioInput key={i} data={data} isUpper={false} highlighted={isHighlighted(d)} disabled />
+              })}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+  render () {
+    let clamp = (frac) => {
+      let steps = 0
+      while (frac[0] / frac[1] > 2.0) {
+        steps++
+        frac[1] *= 2
+        if (steps > 99) {
+          return [null, null]
+        }
+      }
+      while (frac[0] / frac[1] < 1.0) {
+        steps++
+        frac[0] *= 2
+        if (steps > 99) {
+          return [null, null]
+        }
+      }
+      return reduce(frac)
+    }
+    let sanetizeFrac = (frac) => {
+      let a = parseInt(frac[0])
+      let b = parseInt(frac[1])
+      if (isNaN(a) || isNaN(b)) {
+        return null
+      }
+      return [a, b]
+    }
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr><RatioInput data={this.state.main[0]} isUpper changeCB={(v) => {
+              this.setState({main: [v, this.state.main[1]]})
+            }} /></tr>
+            <tr><RatioInput data={this.state.main[1]} changeCB={(v) => {
+              this.setState({main: [this.state.main[0], v]})
+            }} /></tr>
+          </tbody>
+        </table>
 
+        <table>
+          <tbody>
+            <tr>
+              {this.state.ops.map((d, i) => {
+                return <RatioInput key={i} data={d[0]} isUpper changeCB={(v) => {
+                  let ops = clone(this.state.ops)
+                  ops[i][0] = v
+                  this.setState({ ops })
+                }} />
+              })}
+            </tr>
+            <tr>
+              {this.state.ops.map((d, i) => {
+                return <RatioInput key={i} data={d[1]} isUpper={false} changeCB={(v) => {
+                  let ops = clone(this.state.ops)
+                  ops[i][1] = v
+                  this.setState({ ops })
+                }} />
+              })}
+            </tr>
+          </tbody>
+        </table>
 
-				<table>
-					<tbody>
-						<tr>
-							{this.state.ops.map((d, i) => {
-								return <RatioInput key={i} data={d[0]} isUpper={true} changeCB={(v) => {
-									let ops = clone(this.state.ops)
-									ops[i][0] = v
-									this.setState({ ops })
-								}} />
-							})}
-						</tr>
-						<tr>
-							{this.state.ops.map((d, i) => {
-								return <RatioInput key={i} data={d[1]} isUpper={false} changeCB={(v) => {
-									let ops = clone(this.state.ops)
-									ops[i][1] = v
-									this.setState({ ops })
-								}} />
-							})}
-						</tr>
-					</tbody>
-				</table>
+        {this.outList(this.state.ops.map((frac) => {
+          let main = sanetizeFrac(this.state.main)
+          if (!main) {
+            return
+          }
+          return clamp(mul(frac, main))
+        }))}
+        {this.outList(this.state.ops.map((frac) => {
+          let main = sanetizeFrac(this.state.main)
+          if (!main) {
+            return
+          }
+          return clamp(div(main, frac))
+        }))}
 
-				{this.outList(this.state.ops.map((frac) => {
-					let main = sanetizeFrac(this.state.main)
-					if (!main)
-						return
-					return clamp(mul(frac, main))
-				}))}
-				{this.outList(this.state.ops.map((frac) => {
-					let main = sanetizeFrac(this.state.main)
-					if (!main)
-						return
-					return clamp(div(main, frac))
-				}))}
+        <table>
+          <tbody>
+            <tr><RatioInput data={this.state.scnd[0]} isUpper changeCB={(v) => {
+              this.setState({scnd: [v, this.state.scnd[1]]})
+            }} /></tr>
+            <tr><RatioInput data={this.state.scnd[1]} changeCB={(v) => {
+              this.setState({scnd: [this.state.scnd[0], v]})
+            }} /></tr>
+          </tbody>
+        </table>
 
-
-				<table>
-					<tbody>
-						<tr><RatioInput data={this.state.scnd[0]} isUpper={true} changeCB={(v) => {
-							this.setState({scnd: [v, this.state.scnd[1]]})
-						}} /></tr>
-						<tr><RatioInput data={this.state.scnd[1]} changeCB={(v) => {
-							this.setState({scnd: [this.state.scnd[0], v]})
-						}} /></tr>
-					</tbody>
-				</table>
-
-				{this.outList(this.state.ops.map((frac) => {
-					let scnd = sanetizeFrac(this.state.scnd)
-					if (!scnd)
-						return
-					return clamp(mul(frac, scnd))
-				}))}
-				{this.outList(this.state.ops.map((frac) => {
-					let scnd = sanetizeFrac(this.state.scnd)
-					if (!scnd)
-						return
-					return clamp(div(scnd, frac))
-				}))}
-			</div>
-		)
-	}
+        {this.outList(this.state.ops.map((frac) => {
+          let scnd = sanetizeFrac(this.state.scnd)
+          if (!scnd) {
+            return
+          }
+          return clamp(mul(frac, scnd))
+        }))}
+        {this.outList(this.state.ops.map((frac) => {
+          let scnd = sanetizeFrac(this.state.scnd)
+          if (!scnd) {
+            return
+          }
+          return clamp(div(scnd, frac))
+        }))}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-	<Rechner />,
-	document.getElementById('rechner')
-);
+  <Rechner />,
+  document.getElementById('rechner')
+)
