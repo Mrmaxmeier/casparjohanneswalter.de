86884838fec713a7bae55b2adc00eaa5f932d11c
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-08-26 19:11:21 +0200

    simplify webpack require setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-require('file!file?name=[name].[ext]!./index.html')
-require('file!file?name=marx.css!../node_modules/marx-css/css/marx.css')
-require('file!file?name=style.css!sass!./style.scss')
+require('file?name=[name].[ext]!./index.html')
+require('file?name=marx.css!../node_modules/marx-css/css/marx.css')
+require('file?name=style.css!sass!./style.scss')
 
 import React from 'react'
 import ReactDOM from 'react-dom'
