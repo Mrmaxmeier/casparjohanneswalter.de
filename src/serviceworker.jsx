7faf111e59cc4217faf2f93ebd2f2e4b3d354dac
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-01-09 17:08:13 +0100

    refactor serviceworker

--- src/serviceworker.jsx
+++ src/serviceworker.jsx
@@ -5,10 +5,8 @@ export class ServiceWorkerController extends PureComponent {
     super(props)
     this.state = {
       status: '',
-      message: null,
-      online: navigator.onLine || true
+      message: null
     }
-    setInterval(this.update.bind(this), 5000)
   }
 
   componentDidMount () {
@@ -25,21 +23,10 @@ export class ServiceWorkerController extends PureComponent {
     )
   }
 
-  update () {
-    this.setState({online: navigator.onLine || true})
-  }
-
   register () {
-    if ('serviceWorker' in navigator) {
-      // Your service-worker.js *must* be located at the top-level directory relative to your site.
-      // It won't be able to control pages unless it's located at the same level or higher than them.
-      // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-      // See https://github.com/slightlyoff/ServiceWorker/issues/468
+    if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
       navigator.serviceWorker.register('service-worker.js').then((reg) => {
-        // updatefound is fired if service-worker.js changes.
         reg.onupdatefound = () => {
-          // The updatefound event implies that reg.installing is set; see
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
           var installingWorker = reg.installing
 
           installingWorker.onstatechange = () => {
@@ -47,21 +34,15 @@ export class ServiceWorkerController extends PureComponent {
             switch (installingWorker.state) {
               case 'installed':
                 if (navigator.serviceWorker.controller) {
-                  // At this point, the old content will have been purged and the fresh content will
-                  // have been added to the cache.
-                  // It's the perfect time to display a "New content is available; please refresh."
-                  // message in the page's interface.
                   this.setState({
-                    message: 'New or updated content is available.'
+                    message: 'New or updated content is available.',
+                    status: 'update avaliable'
                   })
-                  this.setState({status: 'update available'})
                 } else {
-                  // At this point, everything has been precached.
-                  // It's the perfect time to display a "Content is cached for offline use." message.
                   this.setState({
-                    message: 'Content is cached for offline use' // 'Content is now available offline!'
+                    message: 'Content is cached for offline use',
+                    status: 'offline'
                   })
-                  this.setState({status: 'offline'})
                 }
                 break
 
@@ -72,7 +53,10 @@ export class ServiceWorkerController extends PureComponent {
           }
         }
       }).catch((e) => {
-        this.setState({status: 'error'})
+        this.setState({
+          status: 'error',
+          message: 'Error during service worker registration.'
+        })
         console.error('Error during service worker registration:', e)
       })
     }
