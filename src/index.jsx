41f1be240af7d13910dbc4637217cf61c93d0cf7
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-02-16 18:17:35 +0100

    add sentry error collection

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ require('./static_files.js')
 import React from 'react'
 import { render } from 'react-dom'
 import ReactGA from 'react-ga'
+import Raven from 'raven-js'
 
 import { Routes } from './routes.jsx'
 
 if (__IN_BUILD__) { // eslint-disable-line no-undef
+  Raven.config('https://d0d4207778da4c05a0006fd4ed80322a@sentry.ente.ninja/3').install()
   ReactGA.initialize('UA-39068556-2')
 }
 
