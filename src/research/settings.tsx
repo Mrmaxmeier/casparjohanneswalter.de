390a3a6865237e683090df6ec651518075b145d1
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-07-21 10:59:28 +0200

    fix MathInput Settings

--- src/research/settings.tsx
+++ src/research/settings.tsx
@@ -3,85 +3,56 @@ import * as React from 'react'
 import { clone } from 'lodash'
 
 import { MathInput, NoteDisplay, NoteImage } from './components'
-import { concertPitchToC0, ratioToCents } from './converters.js'
+import { concertPitchToC0, ratioToCents } from './converters'
 
-interface Props {
-  updateState: () => void
+interface Props<V, S> {
+  setState?: (state: S, cb?: () => void) => void,
+  value: V
 }
 
-interface State {
-  value: number
-}
-
-export class Settings extends React.PureComponent<Props, State> {
-  constructor (props: Props) {
+export abstract class Settings<V, S extends V, P={}>
+                extends React.Component<P & Props<V, any>, { override?: V }> {
+  constructor (props: P & Props<S, any>) {
     super(props)
-    this.state = {
-      value: this.cls().default
-    }
+    this.state = { override: undefined }
   }
 
-  onValue (value) {
-    this.setState({ value })
-    if (this.props.updateState) {
-      this.props.updateState(value, this.cls())
-    }
-  }
+  abstract dump(): V
 
-  static state = (...list) => {
-    return list.reduce((v, setting) => {
-      if (typeof setting.default === 'object') {
-        Object.assign(v, setting.default)
-      } else {
-        v[setting.field] = setting.default
-      }
-      return v
-    }, {})
-  }
-
-  static updateState = (component) => {
-    return (val, cls) => {
-      if (typeof cls.default === 'object') {
-        component.setState(val)
-      } else {
-        component.setState({ [cls.field]: val })
-      }
-    }
-  }
-
-  static onPreset = (settings, preset) => {
-    for (let setting of settings) {
-      let cls = setting.cls()
-      if (setting.deserialize) {
-        setting.deserialize(preset)
-      } else if (typeof cls.default === 'object') {
-        setting.setState({value: preset}) // TODO: filter preset keys
-      } else {
-        setting.setState({
-          [cls.field]: preset[cls.field] || cls.default
+  onValue (value: V) {
+    if (this.props.setState)
+      this.props.setState(value, () => {
+        this.setState({ override: undefined }, () => {
+          
         })
-      }
-    }
+      })
   }
 
-  static dumpPreset = (settings, current) => {
-    let state = clone(current)
+  // TODO: variadic generics (O = V & V)
+  static state<V extends O, S extends V, O=any>(settings: Settings<O, S, any>[]): V {
+    let p = {} as V
     for (let setting of settings) {
-      if (setting.serialize) {
-        setting.serialize(state)
-      }
+      Object.assign(p, setting.dump())
     }
-    return state
+    return p
   }
 }
 
-export class ConcertPitchSetting extends Settings {
-  static field = 'concertPitch';
-  static default = 440;
-  cls () { return ConcertPitchSetting }
-  dump () { return this.input.getValue() }
-  serialize (v) { v.concertPitch = this.input.text() }
-  deserialize (preset) { this.input.setValue(preset.concertPitch, true) }
+export interface TConcertPitch { concertPitch: number }
+export class ConcertPitchSetting<S extends TConcertPitch> extends Settings<TConcertPitch, S> {
+  private input?: MathInput
+
+  static default() { return { concertPitch: 440 } }
+
+  dump () {
+    let concertPitch = this.input && this.input.state.value || 440
+    return { concertPitch }
+  }
+  // serialize (v: { concertPitch : string }) { v.concertPitch = this.input.text() }
+  // deserialize (preset: { concertPitch: string }) { this.input.setValue(preset.concertPitch, true) }
+
+  value (): string { return this.input && this.input.text() || '' }
+  setText(s: string) { this.input && this.input.setValue(s, true) }
 
   render () {
     return (
@@ -89,31 +60,44 @@ export class ConcertPitchSetting extends Settings {
         <th>Concert Pitch a4</th>
         <th>
           <MathInput
-            wide asKind="mathjs-ignoreerror"
-            default={440} ref={(e) => { this.input = e }}
-            onChange={this.onValue.bind(this)} />
+            wide
+            default={440} ref={(e) => { if(e) this.input = e }}
+            onChange={(concertPitch) => this.onValue({ concertPitch })} />
         </th>
       </tr>
     )
   }
 }
 
-export class Pitch11Setting extends Settings {
-  static field = 'pitch11';
-  static default = 440 / 9 * 8;
-  cls () { return Pitch11Setting }
-  serialize (v) { v.pitch11 = this.input.text() }
-  deserialize (preset) { this.input.setValue(preset.pitch11, true) }
+
+export interface TPitch11Setting { pitch11: number }
+export interface TPitch11Props { concertPitch: number }
+export class Pitch11Setting<S extends TPitch11Setting> extends Settings<TPitch11Setting, S, TPitch11Props> {
+  private input?: MathInput
+  private default: number = 440 / 9 * 8;
+
+  static default() { return { pitch11: 440 / 9 * 8 } }
+
+  dump () {
+    return { pitch11: this.input && this.input.state.value || this.default }
+  }
+
+  value (): string { return this.input && this.input.text() || '' }
+  setText(s: string) { this.input && this.input.setValue(s, true) }
+
+  // serialize (v: { pitch11 : string }) { v.pitch11 = this.input.text() }
+  // deserialize (preset: { pitch11: string }) { this.input.setValue(preset.pitch11, true) }
   render () {
     let c0 = concertPitchToC0(this.props.concertPitch)
-    let cents = ratioToCents(this.state.value / c0)
+    let cents = ratioToCents(this.dump().pitch11 / c0)
     return (
       <tr>
         <th>Pitch 1 / 1</th>
         <th>
           <MathInput
-            wide asKind="mathjs-ignoreerror" default="440 / 9 * 8"
-            onChange={this.onValue.bind(this)} ref={(e) => { this.input = e }} />
+            wide default="440 / 9 * 8"
+            onChange={(pitch11) => this.onValue({ pitch11 })}
+            ref={(e) => { if (e) this.input = e }} />
         </th>
         <th>
           <NoteImage cents={cents} />
@@ -126,18 +110,27 @@ export class Pitch11Setting extends Settings {
   }
 }
 
-export class RatioCentsModeSetting extends Settings {
-  static field = 'mode';
-  static default = 'ratio';
-  cls () { return RatioCentsModeSetting }
+
+export interface TModeSetting { mode: 'ratio' | 'cents' }
+export class RatioCentsModeSetting<S extends TModeSetting> extends Settings<TModeSetting, S> {
+
+  static default(): TModeSetting { return { mode: 'ratio' } }
+
+  dump () {
+    return this.state.override || this.props.value || { mode: 'ratio' }
+  }
+
   render () {
     return (
       <tr>
         <th>Mode</th>
         <th>
           <select onChange={(e) => {
-            this.onValue(e.target.value)
-          }} value={this.state.mode}>
+            let mode = e.target.value
+            if (mode === 'ratio' || mode === 'cents') {
+              this.onValue({ mode })
+            }
+          }} value={this.dump().mode}>
             <option value="ratio">Ratio</option>
             <option value="cents">Cents</option>
           </select>
@@ -147,22 +140,97 @@ export class RatioCentsModeSetting extends Settings {
   }
 }
 
-export class MutedSetting extends Settings {
-  static field = 'muted';
-  static default = false;
-  cls () { return MutedSetting }
-  serialize (v) { delete v.muted }
+export interface TMutedSetting { muted: boolean }
+export class MutedSetting<S extends TMutedSetting> extends Settings<TMutedSetting, S> {
+  static default() { return { muted: false } }
+  dump() { return this.props.value || { muted: false } }
   render () {
-    let muted = this.state.value
     return (
       <tr>
         <th>Mute</th>
         <th>
           <button onClick={() => {
-            this.onValue(!muted)
-          }}>{muted ? 'un' : ''}mute</button>
+            let muted = !this.dump().muted
+            this.onValue({ muted })
+          }}>{this.dump().muted ? 'un' : ''}mute</button>
         </th>
       </tr>
+    )
+  }
+}
+
+
+
+
+export interface TMemeSetting { meme: 'ratio' | 'cents' }
+export class MemeSetting<S extends TMemeSetting> extends Settings<TMemeSetting, S> {
+  static default(): TMemeSetting { return { meme: 'ratio' } }
+  dump() {
+    return this.props.value || { meme: 'ratio'}
+  }
+  render () {
+    return (
+      <tr>
+        <th>Mode</th>
+        <th>
+          <select onChange={(e) => {
+            let meme = e.target.value
+            if (meme === 'ratio' || meme === 'cents')
+              this.onValue({ meme })
+          }} value={this.dump().meme}>
+            <option value="ratio">Ratio</option>
+            <option value="cents">Cents</option>
+          </select>
+        </th>
+      </tr>
+    )
+  }
+}
+
+export interface TMemeSwitch { isMeme: boolean }
+export class MemeSwitch<S extends TMemeSwitch> extends Settings<TMemeSwitch, S> {
+  static default() { return { isMeme: true } }
+  dump() {
+    return this.props.value || { isMeme: true }
+  }
+  render () {
+    return (null)
+  }
+}
+
+interface TestMemeP { prop: string }
+// export interface TestMemeS { state: number, other: boolean, ...TMemeSetting, ...TMemeSwitch }
+interface TestMemeS extends TMemeSetting, TMemeSwitch {
+  state: number,
+  other: boolean
+}
+class TestMeme extends React.PureComponent<TestMemeP, TestMemeS> {
+  private mset: MemeSetting<TestMemeS>
+  private ismset: MemeSwitch<TestMemeS>
+  constructor (props: TestMemeP) {
+    super(props)
+    // let mset = new MemeSetting<TestMemeS>({})
+    // let ismset = new MemeSwitch<TestMemeS>({})
+    this.state = {
+      /*
+      ...Settings.state<TMemeSetting & TMemeSwitch, TestMemeS>([
+        mset,
+        ismset
+      ]),
+      */
+      ...MemeSetting.default(),
+      ...MemeSwitch.default(),
+      other: true,
+      state: 0
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <MemeSetting setState={this.setState} value={{ meme: this.state.meme }} />
+        <MemeSwitch setState={this.setState} value={{ isMeme: this.state.isMeme }} />
+      </div>
     )
   }
 }
