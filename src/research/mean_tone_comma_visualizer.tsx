75ec3318f63ad9c5bffbaf1cae9c9c25f9be0cf6
Mrmaxmeier <Mrmaxmeier@gmail.com>
2017-10-11 21:12:07 +0200

    mean_tone_viz: zoom range, ...

--- src/research/mean_tone_comma_visualizer.tsx
+++ src/research/mean_tone_comma_visualizer.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { range } from 'lodash'
-import { SpecificRangeSlider } from './components'
+import { SpecificRangeSlider, PrecNumber, MathInput } from './components'
 
-const TEXT = [
+const NOTES = [
     "F♭♭♭♭", "C♭♭♭♭", "G♭♭♭♭", "D♭♭♭♭", "A♭♭♭♭", "E♭♭♭♭",
     "B♭♭♭♭", "F♭♭♭", "C♭♭♭", "G♭♭♭", "D♭♭♭",
     "A♭♭♭", "E♭♭♭", "B♭♭♭", "F♭♭", "C♭♭", "G♭♭",
@@ -20,9 +20,15 @@ const TEXT = [
 interface State {
   fifthCents: number,
   fifthPreset: string,
+  centerIndex: number,
   from: number,
   to: number,
-  zoom: number
+  zoom: number,
+  zoomCenter: number,
+  noteSelection: {
+    from: number
+    to: number
+  }
 }
 
 export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
@@ -30,11 +36,17 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
   constructor (props: {}) {
     super(props)
     this.state = {
-      fifthCents: 700,
-      fifthPreset: '_Preset_',
+      fifthCents: 701.955,
+      fifthPreset: 'perfect fifth (0 comma)',
+      centerIndex: NOTES.findIndex((v) => v === 'C♮'),
       from: 0,
       to: 1200,
-      zoom: 100
+      zoom: 100,
+      zoomCenter: 0,
+      noteSelection: {
+        from: 0,
+        to: NOTES.length - 1
+      }
     }
     this.componentDidUpdate = this.paint.bind(this)
     this.componentDidMount = this.paint.bind(this)
@@ -51,7 +63,10 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
       ctx.font = '20px serif'
       ctx.textAlign = 'center'
       ctx.textBaseline = 'middle'
-      let debug = (x: number, y: number, color?: string) => {
+  
+      // TODO: out-of-bounds
+
+      const debug = (x: number, y: number, color?: string) => {
         ctx.fillStyle = color || 'blue'
         ctx.beginPath()
         const [rX, rY] = stretch(x, y)
@@ -59,10 +74,10 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
         ctx.arc(rX, rY, 5, 0, Math.PI * 2)
         ctx.fill()
       }
+
       const stretch = (x: number, y: number): [number, number] => {
-        // x /= (this.state.from - this.state.to) / 1200
+        x -= this.state.zoomCenter / 600
         x *= this.state.zoom / 100
-        // x += (this.state.from) / 1200
         return [
           (0.9 * x / 2 + 0.5) * canvas.width,
           (0.85 * y / 2 + 0.5) * canvas.height
@@ -73,9 +88,10 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
 
       ctx.fillStyle = 'black'
       ctx.font = '18px serif'
+      ctx.lineWidth = 2
       ctx.beginPath()
       ctx.moveTo.apply(ctx, stretch(-1, 1))
-      ctx.lineTo.apply(ctx, stretch( 1, 1))
+      ctx.lineTo.apply(ctx, stretch( 3, 1))
       ctx.stroke()
       range(0, 1200, 100).forEach((cents, index) => {
         let posX = ((cents + 600) % 1200) / 600 - 1
@@ -84,18 +100,18 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
           ctx.lineWidth = 3
           ctx.fillStyle = 'black'
           ctx.beginPath()
-          ctx.moveTo.apply(ctx, stretch(shift + posX, posY + 3 / TEXT.length))
-          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 3 / TEXT.length))
+          ctx.moveTo.apply(ctx, stretch(shift + posX, posY + 3 / NOTES.length))
+          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 3 / NOTES.length))
           ctx.stroke()
 
-          const [x, y] = stretch(posX + shift, posY - 5 / TEXT.length)
+          const [x, y] = stretch(posX + shift, posY - 5 / NOTES.length)
           ctx.fillText(cents.toString(), x, y);
 
           ctx.fillStyle = 'gray'
           ctx.lineWidth = 0.5
           ctx.beginPath()
           ctx.moveTo.apply(ctx, stretch(shift + posX, -1))
-          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 10 / TEXT.length))
+          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 10 / NOTES.length))
           ctx.stroke()
         })
       })
@@ -132,47 +148,49 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
         let posX = ((cents + 600) % 1200) / 600 - 1
         let posY = 1
         ;[0, 2].forEach(shift => {
-          let [x, y] = stretch(posX + shift, posY + 5 / TEXT.length)
+          let [x, y] = stretch(posX + shift, posY + 5 / NOTES.length)
           ctx.fillText(a.toString(), x, y);
-          [x, y] = stretch(posX + shift, posY + 10 / TEXT.length)
+          [x, y] = stretch(posX + shift, posY + 10 / NOTES.length)
           ctx.fillText(b.toString(), x, y)
 
           ctx.strokeStyle = 'red'
           ctx.lineWidth = 0.5
           ctx.beginPath()
           ctx.moveTo.apply(ctx, stretch(shift + posX, -1))
-          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 10 / TEXT.length))
+          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 10 / NOTES.length))
           ctx.stroke()
         })
       })
 
-      let centerIndex = 29
-      let centerX = (this.state.fifthCents * centerIndex) % 1200 / 600 - 1
-      let centerY = 1 - (centerIndex / (TEXT.length / 2))
+      let centerX = (this.state.fifthCents * this.state.centerIndex) % 1200 / 600 - 1
+      let centerY = 1 - (this.state.centerIndex / (NOTES.length / 2))
 
       ctx.fillStyle = 'black'
       ctx.strokeStyle = 'black'
       ctx.lineWidth = 3
-      TEXT.forEach((label, index) => {
+      NOTES.forEach((label, index) => {
+        if (this.state.noteSelection.from > index) { return }
+        if (this.state.noteSelection.to   < index) { return }
+
         let cents = this.state.fifthCents * index
         let posX = (cents % 1200) / 600 - 1
-        let posY = 1 - (index / (TEXT.length / 2))
+        let posY = 1 - (index / (NOTES.length / 2))
         posX -= centerX
         posY -= centerY
         posY *= 0.8
-        ;[-2, 0, 2].forEach((shift, idx) => {
+        ;[-2, 0, 2, 4].forEach((shift, idx) => {
           // debug(shift + posX, posY, 'black')
           const [x, y] = stretch(shift + posX, posY)
 
           // TODO: tuple spread
           ctx.beginPath()
-          ctx.moveTo.apply(ctx, stretch(shift + posX, posY + 2 / TEXT.length))
-          ctx.lineTo.apply(ctx, stretch(shift + posX, posY + 6 / TEXT.length))
+          ctx.moveTo.apply(ctx, stretch(shift + posX, posY + 2 / NOTES.length))
+          ctx.lineTo.apply(ctx, stretch(shift + posX, posY + 6 / NOTES.length))
           ctx.stroke()
 
           ctx.beginPath()
-          ctx.moveTo.apply(ctx, stretch(shift + posX, posY - 2 / TEXT.length))
-          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 6 / TEXT.length))
+          ctx.moveTo.apply(ctx, stretch(shift + posX, posY - 2 / NOTES.length))
+          ctx.lineTo.apply(ctx, stretch(shift + posX, posY - 6 / NOTES.length))
           ctx.stroke()
           ctx.fillText(label, x, y);
         })
@@ -190,18 +208,19 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
   render () {
     let size = { width: '750px', height: '500px' }
     const centPresets: { [key: string]: number } = {
-      "EDO 19": 694.74,
-      "1/3 comma": 694.79,
-      "2/7 comma": 695.81,
-      "1/4 comma": 696.58,
-      "EDO 31": 696.77,
-      "1/5 comma": 697.65,
-      "1/6 comma": 698.37,
+      "EDO 19": 694.737,
+      "1/3 comma": 694.786,
+      "2/7 comma": 695.810,
+      "1/4 comma": 696.578,
+      "EDO 31": 696.774,
+      "1/5 comma": 697.654,
+      "1/6 comma": 698.371,
       "EDO 12": 700,
-      "EDO 53": 701.89,
-      "perfect fifth": 701.96,
-      "optimized 7": 702.23,
-      "EDO 34": 705.88
+      "shismatic pythagorean, optimized third": 701.711,
+      "EDO 53": 701.887,
+      "perfect fifth (0 comma)": 701.955,
+      "shismatic pythagorean, optimized seventh": 702.227,
+      "EDO 34": 705.882
     }
     return (
       <div>
@@ -209,20 +228,35 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
           <tbody>
             <tr>
               <th>Fifth</th>
-              <td>{this.state.fifthCents}</td>
               <td>
-                <SpecificRangeSlider defaultMin={694} defaultMax={706} onChange={(val) => {
+                <input
+                  type="number"
+                  value={this.state.fifthCents}
+                  step={0.001}
+                  style={{width: '6em'}}
+                  onChange={(e) => this.setState({
+                    fifthCents: parseFloat(e.target.value)
+                  })}
+                />
+              </td>
+              <td>
+                <SpecificRangeSlider defaultMin={694} defaultMax={706} step={0.001} onChange={(val) => {
                   this.setState({ fifthCents: val, fifthPreset: '_Preset_' })
                 }}/>
               </td>
+            </tr>
+            <tr>
+              <th>Fifth from preset</th>
+              <th />
               <td>
                 <select value={this.state.fifthPreset} onChange={(e) => {
                   let key = e.target.value
+                  if (key === '_Preset_') { return }
                   this.setState({ fifthCents: centPresets[key], fifthPreset: key })
                 }}>
                   <option value="_Preset_">Preset</option>
                   {Object.keys(centPresets).map(key =>
-                    <option value={key} key={key}>{key}</option>
+                    <option value={key} key={key}>{key} - {centPresets[key]}</option>
                   )}
                 </select>
               </td>
@@ -234,6 +268,56 @@ export class MeanToneCommaVisualizer extends React.PureComponent<{}, State> {
                 <SpecificRangeSlider defaultValue={100} defaultMin={100} defaultMax={600} onChange={(val) => {
                   this.setState({ zoom: val })
                 }}/>
+              </td>
+            </tr>
+            <tr>
+              <th>Center of Zoom</th>
+              <td><PrecNumber digits={4} precision={0} value={this.state.zoomCenter} /> cents</td>
+              <td>
+                <SpecificRangeSlider defaultValue={0} defaultMin={0} defaultMax={1200} onChange={(val) => {
+                  this.setState({ zoomCenter: val })
+                }}/>
+              </td>
+            </tr>
+            <tr>
+              <th>Center of fifths</th>
+              <td>
+                <select value={NOTES[this.state.centerIndex]} onChange={(e) => {
+                    let key = e.target.value
+                    this.setState({ centerIndex: NOTES.findIndex((val) => val === key) })
+                  }}>
+                  {['C♮', 'G♮', 'D♮', 'A♮'].map(key =>
+                    <option value={key} key={key}>{key}</option>
+                  )}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <th>Visible note range</th>
+              <td>
+                <select value={this.state.noteSelection.from} onChange={(e) => {
+                    let noteSelection = Object.assign({}, this.state.noteSelection)
+                    noteSelection.from = parseInt(e.target.value)
+                    noteSelection.to = Math.max(noteSelection.from, noteSelection.to)
+                    this.setState({ noteSelection })
+                  }}>
+                  {NOTES.map((text, idx) =>
+                    <option value={idx} key={idx}>{text}</option>
+                  )}
+                </select>
+                {' to '}
+              </td>
+              <td>
+                <select value={this.state.noteSelection.to} onChange={(e) => {
+                    let noteSelection = Object.assign({}, this.state.noteSelection)
+                    noteSelection.to = parseInt(e.target.value)
+                    noteSelection.from = Math.min(noteSelection.from, noteSelection.to)
+                    this.setState({ noteSelection })
+                  }}>
+                  {NOTES.map((text, idx) =>
+                    <option value={idx} key={idx}>{text}</option>
+                  )}
+                </select>
               </td>
             </tr>
           </tbody>
