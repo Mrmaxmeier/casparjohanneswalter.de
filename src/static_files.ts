02e587c4bae362f7bc394f45aa0987138a0f1210
Mrmaxmeier <Mrmaxmeier@gmail.com>
2019-02-17 18:55:49 +0100

    cleanup

--- src/static_files.ts
+++ src/static_files.ts
@@ -1,3 +1,5 @@
 require<string>('file-loader?name=[name].[ext]!./index.html')
 require<string>('file-loader?name=style.css!sass-loader!./style.scss')
 require<string>('file-loader?name=robots.txt!../static/robots.txt')
+require<string>('file-loader?name=marx.min.css!../node_modules/marx-css/css/marx.min.css')
+require<string>('file-loader?name=marx.min.css.map!../node_modules/marx-css/css/marx.min.css.map')
